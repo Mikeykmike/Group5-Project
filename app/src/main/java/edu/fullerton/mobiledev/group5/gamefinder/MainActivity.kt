b32b8156afc2402ad328e740323b580f4434c528
Matt Watkins <mattwatkins@gmail.com>
2021-10-21 13:34:11 -0700

    Added game detail page code

--- app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/MainActivity.kt
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/MainActivity.kt
@@ -2,8 +2,10 @@ package edu.fullerton.mobiledev.group5.gamefinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationBarView
 import edu.fullerton.mobiledev.group5.gamefinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,26 @@ class MainActivity : AppCompatActivity() {
 
         val mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+//        val controller = findNavController(R.id.navigationController)
+//        NavigationUI.setupActionBarWithNavController(this, controller)
+//
+//        mainBinding.bottomNavigationBar.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener(
+//            fun(it: MenuItem) : Boolean {
+//                when (it.itemId)
+//                {
+//                    R.id.trending -> {
+//                        controller.navigate(R.id.gameDetail)
+//                        return true
+//                    }
+//
+//                }
+//                return true
+//            }))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val controller = findNavController(R.id.navigationController)
+        return controller.navigateUp()
     }
 }
