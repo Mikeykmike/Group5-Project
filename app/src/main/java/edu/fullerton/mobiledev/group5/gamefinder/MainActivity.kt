a5248895130080851b43918828fad0ca688d4db3
Mikester <Mikeykmike@gmail.com>
2021-10-21 15:10:02 -0700

    Favorites page added

--- app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/MainActivity.kt
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/MainActivity.kt
@@ -1,12 +1,18 @@
 package edu.fullerton.mobiledev.group5.gamefinder
 
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import edu.fullerton.mobiledev.group5.gamefinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val favFragment = Favorites()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,4 +22,13 @@ class MainActivity : AppCompatActivity() {
         mainBinding.recyclerView.layoutManager = GridLayoutManager(this,4)
         mainBinding.recyclerView.adapter = GameViewAdapter()
     }
+
+    private fun switchActivity(fragment: Fragment){
+        if(fragment != null) {
+            val transaction= supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragContainer,fragment)
+            transaction.commit()
+        }
+    }
+
 }
