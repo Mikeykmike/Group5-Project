9af93cc784e93b33c133aa970fa6327382f9d43d
Matt Watkins <mattwatkins@gmail.com>
2021-11-04 17:03:26 -0700

    Add RAWG game api code

--- app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/GameViewHolder.kt
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/GameViewHolder.kt
@@ -1,5 +1,6 @@
 package edu.fullerton.mobiledev.group5.gamefinder
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,12 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class GameViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
     val image: ImageView = view.findViewById(R.id.imageView)
@@ -23,12 +30,38 @@ class GameViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickLi
 
 class GameViewAdapter : RecyclerView.Adapter<GameViewHolder>()
 {
+    var gameList: GameList = GameList(0, "","", listOf())
+    init {
+        val client = OkHttpClient.Builder().build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(VideoGameDatabaseService.API_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(VideoGameDatabaseService::class.java)
+
+        service.getGames().enqueue(object: Callback<GameList> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(call: Call<GameList>, response: Response<GameList>) {
+                if (response.isSuccessful) {
+                    gameList = response.body() ?: gameList
+                    notifyDataSetChanged()
+                }
+            }
+
+            override fun onFailure(call: Call<GameList>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder =
         GameViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.game_image_widget, parent, false))
 
     override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
-        holder.text.text = "Game $position"
+        holder.text.text = gameList.results[position].name
     }
 
-    override fun getItemCount(): Int = 100
+    override fun getItemCount(): Int = gameList.results.size
 }
