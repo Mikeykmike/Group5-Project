392a118ab5a7c7b410045e56d84945cd25d05520
dramirez2426 <46946234+dramirez2426@users.noreply.github.com>
2021-11-04 17:39:47 -0700

    updated login screen and added registration page
    added stencil to create database that learns users
    - updated stencil for the variables in registration page

--- app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/MainActivity.kt
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/MainActivity.kt
@@ -3,18 +3,30 @@ package edu.fullerton.mobiledev.group5.gamefinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.navigation.NavigationBarView
+import edu.fullerton.mobiledev.group5.gamefinder.database.ProfileDatabase
 import edu.fullerton.mobiledev.group5.gamefinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val mainBinding = ActivityMainBinding.inflate(layoutInflater)
+       val mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        val navFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val controller = navFragment.navController
+        val config = AppBarConfiguration(controller.graph)
+        setupActionBarWithNavController(controller, config)
 //        val controller = findNavController(R.id.navigationController)
 //        NavigationUI.setupActionBarWithNavController(this, controller)
 //
@@ -29,8 +41,27 @@ class MainActivity : AppCompatActivity() {
 //
 //                }
 //                return true
-//            }))
+//            }
+        val application = requireNotNull(this).application
+
+        // Retrieve Intersection data access object.
+        val dataSource = ProfileDatabase.getInstance(application).profileDao
+
+        // Create a factory that generates IntersectionViewModels connected to the database.
+        val viewModelFactory = ProfileViewModelFactory(dataSource, application)
+
+        // Generate an ViewModel using the factory.
+        val profileViewModel =
+            ViewModelProvider(
+                this, viewModelFactory
+            ).get(ProfileViewModel::class.java)
+
+        // Connect the IntersectionViewModel with the variable in the layout
+       // mainBinding.profileViewModel = profileViewModel
+        // Assign the lifecycle owner to the activity so it manages the data accordingly.
+        //mainBinding.lifecycleOwner = this
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         val controller = findNavController(R.id.navigationController)
