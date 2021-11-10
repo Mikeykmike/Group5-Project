dea7ba5dab366911097e646261d3afc342b22d5d
Mikester <Mikeykmike@gmail.com>
2021-11-09 17:32:30 -0800

    Added navigation to Fav,Search,Trending

--- app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/MainActivity.kt
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/MainActivity.kt
@@ -3,18 +3,33 @@ package edu.fullerton.mobiledev.group5.gamefinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.navigation.NavigationBarView
 import edu.fullerton.mobiledev.group5.gamefinder.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_game_browser.*
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class MainActivity : AppCompatActivity() {
+    private val favorites = favorites()
+    private val search = search()
+    private val trending = trending()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        bottomNavigationBar.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.favorites -> replaceFragment(favorites)
+                R.id.search -> replaceFragment(search)
+                R.id.trending -> replaceFragment(trending)
+            }
+            true
+        }
 //        val controller = findNavController(R.id.navigationController)
 //        NavigationUI.setupActionBarWithNavController(this, controller)
 //
@@ -30,6 +45,15 @@ class MainActivity : AppCompatActivity() {
 //                }
 //                return true
 //            }))
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if (fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragContainer, fragment)
+            transaction.commit()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
