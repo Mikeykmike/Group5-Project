a1e13ee787210e4f320ca91d6cfb844c0e89f60f
Jasmine Nguyen <msjas@csu.fullerton.edu>
2021-11-15 14:23:19 -0800

    searchbar

--- app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/GameViewHolder.kt
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/GameViewHolder.kt
@@ -1,4 +1,4 @@
-//package edu.fullerton.mobiledev.group5.gamefinder
+package edu.fullerton.mobiledev.group5.gamefinder
 
 import android.view.LayoutInflater
 import android.view.View
@@ -7,19 +7,19 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-//class GameViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-   // val image: ImageView = view.findViewById(R.id.imageView)
-   // val text: TextView = view.findViewById(R.id.textView)
-//}
+class GameViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+   val image: ImageView = view.findViewById(R.id.imageView)
+    val text: TextView = view.findViewById(R.id.textView)
+}
 
-//class GameViewAdapter : RecyclerView.Adapter<GameViewHolder>()
-//{
-  //  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder =
-    //    GameViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.game_image_widget, parent, false))
+class GameViewAdapter : RecyclerView.Adapter<GameViewHolder>()
+{
+   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder =
+        GameViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.game_image_widget, parent, false))
 
-   // override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
-    //    holder.text.text = "Game $position"
-   // }
+   override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+        holder.text.text = "Game $position"
+    }
 
-   // override fun getItemCount(): Int = 100
-//}
+    override fun getItemCount(): Int = 100
+}
