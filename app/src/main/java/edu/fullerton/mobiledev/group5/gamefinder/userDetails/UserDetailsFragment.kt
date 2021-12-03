b879a19f829e39105860128d4d9dc144b1fa8788
dramirez2426 <46946234+dramirez2426@users.noreply.github.com>
2021-12-02 17:18:13 -0800

    created new fragments for login and registration updated database added view to show user details for testing
    must debug errors concerning databinding and navigation

---
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/userDetails/UserDetailsFragment.kt
@@ -0,0 +1,75 @@
+package edu.fullerton.mobiledev.group5.gamefinder.userDetails
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import edu.fullerton.mobiledev.group5.gamefinder.R
+import edu.fullerton.mobiledev.group5.gamefinder.database.RegisterDatabase
+import edu.fullerton.mobiledev.group5.gamefinder.database.RegisterRepository
+import edu.fullerton.mobiledev.group5.gamefinder.databinding.UserDetailsFragmentBinding
+
+class UserDetailsFragment : Fragment() {
+
+    private lateinit var userDetailsViewModel: UserDetailsViewModel
+    private lateinit var mainBinding: UserDetailsFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mainBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_user_details, container, false
+        )
+
+        val application = requireNotNull(this.activity).application
+
+        val dao = RegisterDatabase.getInstance(application).registerDatabaseDao
+
+        val repository = RegisterRepository(dao)
+
+        val factory = UserDetailsFactory(repository, application)
+
+        userDetailsViewModel =
+            ViewModelProvider(this, factory).get(UserDetailsViewModel::class.java)
+
+        mainBinding.userDelailsLayout = userDetailsViewModel
+
+        userDetailsViewModel.navigateto.observe(viewLifecycleOwner, Observer { hasFinished ->
+            if (hasFinished == true) {
+                val action = UserDetailsFragmentDirections.actionUserDetailsFragmentToLoginFragment()
+                NavHostFragment.findNavController(this).navigate(action)
+                userDetailsViewModel.doneNavigating()
+            }
+        })
+
+        initRecyclerView()
+
+        return mainBinding.root
+
+    }
+
+
+    private fun initRecyclerView() {
+        mainBinding.usersRecyclerView.layoutManager = LinearLayoutManager(this.context)
+        displayUsersList()
+    }
+
+
+    private fun displayUsersList() {
+        Log.i("MYTAG", "Inside ...UserDetails..Fragment")
+        userDetailsViewModel.users.observe(viewLifecycleOwner, Observer {
+            mainBinding.usersRecyclerView.adapter = MyRecycleViewAdapter(it)
+        })
+
+    }
+
+}
