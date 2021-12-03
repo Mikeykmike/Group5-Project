f7bd13544bbd5f2c6ef2373e2f8780a8f090cd26
Matt Watkins <mattwatkins@gmail.com>
2021-12-02 19:00:19 -0800

    Fix warnings

--- app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/GameBrowser.kt
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/GameBrowser.kt
@@ -6,19 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
-import edu.fullerton.mobiledev.group5.gamefinder.databinding.ActivityMainBinding
 import edu.fullerton.mobiledev.group5.gamefinder.databinding.FragmentGameBrowserBinding
-import okhttp3.OkHttpClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 class GameBrowser : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
