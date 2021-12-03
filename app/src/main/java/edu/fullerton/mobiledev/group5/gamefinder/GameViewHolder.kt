5bc073370d236d14ecd029578738fcd4c1d099fe
Matt Watkins <mattwatkins@gmail.com>
2021-12-02 18:59:38 -0800

    Update to pass id as arg

--- app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/GameViewHolder.kt
+++ app/src/main/java/edu/fullerton/mobiledev/group5/gamefinder/GameViewHolder.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.os.bundleOf
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -19,13 +18,14 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class GameViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
-    val image: ImageView = view.findViewById(R.id.imageView)
-    val text: TextView = view.findViewById(R.id.textView)
+    private val imageView: ImageView = view.findViewById(R.id.imageView)
+    val textView: TextView = view.findViewById(R.id.textView)
+    var id: Int = 0
 
     var imageUrl: String = ""
         set(value) {
             if (value.isNotEmpty()) {
-                Glide.with(itemView.context).load(value).centerCrop().apply(RequestOptions().override(200, 200)).into(image)
+                Glide.with(itemView.context).load(value).centerCrop().apply(RequestOptions().override(200, 200)).into(imageView)
             }
             field = value
         }
@@ -36,8 +36,9 @@ class GameViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickLi
 
     override fun onClick(view: View) {
         val args = GameBrowserDirections.actionGameBrowserToGameDetail()
-        args.gameTitle = text.text.toString()
+        args.gameTitle = textView.text.toString()
         args.imageUrl = imageUrl
+        args.id = id
         findNavController(view).navigate(args)
     }
 }
@@ -81,7 +82,8 @@ class GameViewAdapter : RecyclerView.Adapter<GameViewHolder>() {
         )
 
     override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
-        holder.text.text = gameList.results[position].name
+        holder.id = gameList.results[position].id
+        holder.textView.text = gameList.results[position].name
         val url = gameList.results[position].imageUrl
         holder.imageUrl = url ?: holder.imageUrl
     }
